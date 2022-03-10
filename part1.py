8264ad5b2e490137fef7db4badab4665384f5f86
Mike <mikebio@gmail.com>
2022-03-10 21:55:39 +0000

    Completed part 1

---
+++ part1.py
@@ -0,0 +1,9 @@
+import math
+
+def print_division_info(num1, num2):
+  div = math.floor(num1/num2)
+  rem = "XXXX"
+  print (str(num1) + " divided by " + str(num2) + " is " + str(div) + " remainder " + rem)
+
+print_division_info(4, 3)  # should print: 4 divided by 3 is 1 remainder 1
+print_division_info(42, 12)  # should print: 42 divided by 12 is 3 remainder 6
